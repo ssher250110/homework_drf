fc8e8ef1abbc73c7a0ad32724f17d6ead47aeb03
Sergey Shershikov <ssher250110@yandex.ru>
2024-05-13 01:24:40 +0300

    added template settings users

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'users',
     'rest_framework',
 ]
 
@@ -114,3 +114,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
