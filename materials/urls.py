7eb49bfa0e0ba7f4c66e0f97a9b31c9d71dc576c
Sergey Shershikov <ssher250110@yandex.ru>
2024-05-13 16:11:19 +0300

    added serializer, viewset and router for Course model

--- materials/urls.py
+++ materials/urls.py
@@ -1,7 +1,13 @@
+from rest_framework.routers import SimpleRouter
+
 from materials.apps import MaterialsConfig
+from materials.views import CourseViewSet
+
+router = SimpleRouter()
+router.register('', CourseViewSet)
 
 app_name = MaterialsConfig.name
 
 urlpatterns = [
 
-]
+] + router.urls
