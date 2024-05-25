3a491238993889aa9459eac36c4fecd3539abb0a
ssher <ssher250110@yandex.ru>
2024-05-25 16:16:18 +0300

    added ordering fields

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from rest_framework import filters
 from rest_framework.generics import ListAPIView
 
 from users.models import Payment
@@ -7,3 +8,5 @@ from users.serializers import PaymentSerializer
 class PaymentListAPIView(ListAPIView):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ('date_payment',)
