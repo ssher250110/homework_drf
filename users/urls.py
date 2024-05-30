1ff35c80d71d03512db712f38227d981854e2474
ssher <ssher250110@yandex.ru>
2024-05-30 21:11:20 +0300

    added settings jwt, token lifetime and urls

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
 from users.views import PaymentListAPIView
@@ -7,4 +8,6 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path('payments/', PaymentListAPIView.as_view(), name='payments_list'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
