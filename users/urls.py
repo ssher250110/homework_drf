696640bae77ad2a2a6787f083e729f19bd098cbf
ssher <ssher250110@yandex.ru>
2024-05-30 21:11:51 +0300

    added settings jwt, token lifetime and urls ver.2

--- users/urls.py
+++ users/urls.py
@@ -8,6 +8,6 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path('payments/', PaymentListAPIView.as_view(), name='payments_list'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
