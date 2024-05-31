98ac92f71926bb82afdaf1167ba94fb28acacb00
ssher <ssher250110@yandex.ru>
2024-06-01 01:55:37 +0300

    added admin view Subscribe

--- materials/admin.py
+++ materials/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from materials.models import Course, Lesson
+from materials.models import Course, Lesson, Subscribe
 
 admin.site.register(Course)
 admin.site.register(Lesson)
+admin.site.register(Subscribe)
